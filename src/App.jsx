9babccacba413e288508f66e917a132415119532
priyanka singh <piru.29072k@gmail.com>
2024-06-16 19:28:35 +0530

    router set

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./components/Home";
+import Add from "./components/Add";
+import Edit from "./components/Edit";
+import Show from "./components/Show";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
     <>
-      <p>hii.....</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/show" element={<Show />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
